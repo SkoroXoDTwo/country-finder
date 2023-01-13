3e9a2c443f72058e0403c78d4f7b8860180609b5
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-14 01:29:50 +0500

    feat: подготовить стартовый шаблон проекта

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__container"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./vendor/normalize.scss";
+import "./vendor/fonts.scss";
+import "./index.scss";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
