2adc53186f58178b95a79f4ab0684e91a165de19
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-14 18:47:26 +0500

    feat: сверстать список фильтра

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,12 +1,28 @@
 import "./Filters.scss";
+import { regionsConfig } from "../../mock/regionsConfig";
+import { useState } from "react";
 
 function Filters() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleOpenList = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="filters">
-      <button className="filters__btn">
+      <button className="filters__btn" onClick={handleOpenList}>
         <p className="filters__btn-text">Filter by Region</p>
-        <span className="filters__btn-icon"></span>
+        <span className={`filters__btn-icon ${isOpen ? "filters__btn-icon_opened" : ''}`}></span>
       </button>
+
+      <ul className={`filters__list ${isOpen ? "filters__list_opened" : ''}`}>
+          {regionsConfig.map((region, index) => (
+            <li key={index}>
+              <button className="filters__list-item">{region}</button>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
---
+++ src/mock/regionsConfig.js
@@ -0,0 +1,3 @@
+export const regionsConfig = [
+  "africa", "america", "asia", "europe", "oceania"
+]
