946c658ced879c0ca73efea346ba4804e825ef23
Alexey Belkov <lexabelco2013@yandex.ru>
2023-01-14 17:17:55 +0500

    feat: создать компонент Filters

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,14 @@
+import "./Filters.scss";
+
+function Filters() {
+  return (
+    <div className="filters">
+      <button className="filters__btn">
+        <p className="filters__btn-text">Filter by Region</p>
+        <span className="filters__btn-icon"></span>
+      </button>
+    </div>
+  );
+}
+
+export default Filters;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import "./Navbar.scss"
 import Search from "../Search/Search";
+import Filters from "../Filters/Filters";
 
 function Navbar() {
   return (
     <section className="navbar">
       <Search />
+      <Filters />
     </section>
   )
 }
